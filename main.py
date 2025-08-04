e9903951f7e8917ed51bf0f828f59ae19ebaa227
Zeeshan Asim <zeeshanravian1@gmail.com>
2025-08-04 22:43:52 +0500

    changing main.py file

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':'Subhan Zahoor'}} 
 
-@app.get('/about')
-def about():
-    return {'data' : 'about page'}
+@app.get('/blog')
+def index(limit = 12 , published : bool = True,sort : Optional[str] = None):
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    else:
+        return {'data': f"{limit} blogs from the db"}
 
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all blogs are unpublished"}
+
+@app.get('/blog/{id}')
+def about(id:int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data':{'1','2'}}
+ 
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all blogs are unpublished'}
