01ea7339599a629b31bfc0585c0672f785d6173d
Zeeshan Asim <zeeshanravian1@gmail.com>
2025-08-06 19:03:02 +0500

    adding new database

---
+++ blog/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI,Depends
+from sqlalchemy.orm import Session
+from . import models, schemas
+from .database import engine,SessionLocal
+
+app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()    
+
+
+@app.post("/blog")
+def create(request: schemas.Blog, db : Session = Depends(get_db)):
+    new_blog = models.Blog(title = request.title,body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
