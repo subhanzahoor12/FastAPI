107abaa7c6aa6fd4cd7ffc4d063b03778e27aa5e
Zeeshan Asim <zeeshanravian1@gmail.com>
2025-08-07 21:14:24 +0500

    editing files

--- blog/main.py
+++ blog/main.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 from . import models, schemas
 from .database import SessionLocal, engine
-from passlib.context import CryptContext
+from .hashing import Hash
 app = FastAPI()
 
 # models.Base.metadata.drop_all(engine)
@@ -18,22 +18,24 @@ def get_db():
         db.close()
 
 
-@app.post("/blog", status_code=status.HTTP_201_CREATED)
+@app.post("/blog", status_code=status.HTTP_201_CREATED,tags = ['blogs'])
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+    new_blog = models.Blog(title=request.title, body=request.body,user_id = 1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
 
-@app.get("/blog", response_model=list[schemas.ShowBlog])
+@app.get("/blog", response_model=list[schemas.ShowBlog], tags=["blogs"])
 def all(db: Session = Depends(get_db)):
     blog = db.query(models.Blog).all()
     return blog
 
 
-@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete(
+    "/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["blogs"]
+)
 def destroy(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -46,7 +48,12 @@ def destroy(id, db: Session = Depends(get_db)):
     return "done"
 
 
-@app.get("/blog/{id}", status_code=200, response_model=schemas.ShowBlog)
+@app.get(
+    "/blog/{id}",
+    status_code=200,
+    response_model=schemas.ShowBlog,
+    tags=["blogs"],
+)
 def get_one(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -60,7 +67,7 @@ def get_one(id, response: Response, db: Session = Depends(get_db)):
     return blog
 
 
-@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED, tags=["blogs"])
 def update_data(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -72,16 +79,23 @@ def update_data(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     return "updated"
 
-pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@app.post('/user')
+@app.post('/user',response_model=schemas.ShowUser,tags = ['users'])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    hashed_password = pwd_cxt.hash(request.email)
     new_user = models.User(
-        name=request.name, email=request.email, password=hashed_password
+        name=request.name, email=request.email, password=Hash.bcrypt(request.password)
         )
     
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+@app.get("/user/{id}", response_model=schemas.ShowUser, tags=["users"])
+def get_user(id:int,db:Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"user with id {id} is not available")
+    return user
+
+
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import Column, Integer, String,ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
 
@@ -9,6 +9,8 @@ class Blog(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer,ForeignKey('users.id'))
+    creator = relationship('User',back_populates='blogs')
 
 
 class User(Base):
@@ -18,3 +20,4 @@ class User(Base):
     email = Column(String)
     name = Column(String)
     password = Column(String)
+    blogs= relationship('Blog',back_populates='creator')
