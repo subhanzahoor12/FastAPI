b326b3d0db346a89a177e84c01a2f229fd624d27
Zeeshan Asim <zeeshanravian1@gmail.com>
2025-08-08 22:42:43 +0500

    editing files

--- blog/main.py
+++ blog/main.py
@@ -1,104 +1,17 @@
-from fastapi import Depends, FastAPI, HTTPException, Response, status
-from sqlalchemy.orm import Session
-from typing import List
-from . import models, schemas
-from .database import  engine,get_db
-from .hashing import Hash
-from .routers import blog
+from fastapi import  FastAPI
+from .database import  engine
+from . import models
+from .routers import blog,user,authentication
 app = FastAPI()
 
 # models.Base.metadata.drop_all(engine)
 models.Base.metadata.create_all(engine)
 
+app.include_router(authentication.router)
 app.include_router(blog.router)
+app.include_router(user.router)
 
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
 
-
-@app.post("/blog", status_code=status.HTTP_201_CREATED,tags = ['blogs'])
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body,user_id = 1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
-
-
-# @app.get("/blog", response_model=list[schemas.ShowBlog], tags=["blogs"])
-# def all(db: Session = Depends(get_db)):
-#     blog = db.query(models.Blog).all()
-#     return blog
-
-
-@app.delete(
-    "/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["blogs"]
-)
-def destroy(id, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with id : {id} is not availaible",
-        )
-    blog.delete(synchronize_session=False)
-    db.commit()
-    return "done"
-
-
-@app.get(
-    "/blog/{id}",
-    status_code=200,
-    response_model=schemas.ShowBlog,
-    tags=["blogs"],
-)
-def get_one(id, response: Response, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Blog with id : {id} is not availaible",
-        )
-    # response.status_code = status.HTTP_404_NOT_FOUND
-    # return {'detail': f"Blog with id : {id} is not availaible"}
-
-    return blog
-
-
-@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED, tags=["blogs"])
-def update_data(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"blog with id {id} not found",
-        )
-    blog.update(request.dict())
-    db.commit()
-    return "updated"
-
-
-@app.post('/user',response_model=schemas.ShowUser,tags = ['users'])
-def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(
-        name=request.name, email=request.email, password=Hash.bcrypt(request.password)
-        )
-    
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-
-@app.get("/user/{id}", response_model=schemas.ShowUser, tags=["users"])
-def get_user(id:int,db:Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"user with id {id} is not available")
-    return user
 
 
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List,Optional 
 
 
 class BlogBase(BaseModel):
@@ -30,4 +30,18 @@ class ShowBlog(BaseModel):
     creator : ShowUser
 
     class Config():
-        orm_mode = True        
+        orm_mode = True    
+
+
+class Login(BaseModel):
+    username: str
+    password: str 
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str]= None               
